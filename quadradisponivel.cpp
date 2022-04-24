270cf5bfd59bd71b783ec61bd8ec6af04fad747d
janainareis1 <103438459+janainareis1@users.noreply.github.com>
2022-04-24 20:45:18 -0300

    Add files via upload

---
+++ quadradisponivel.cpp
@@ -0,0 +1,28 @@
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+
+    setlocale(LC_ALL, "portuguese");   
+
+	//char quad_alpha = 'Quadra Alpha', quad_bravo = 'Quadra Bravo', quad_charley ='Quadra Charley', quad_elgar = 'Quadra Elgar', quad_frey = 'Quadra Frey', quad_garen = 'Quadra Garen';
+	char quad_alpha = 0, quad_bravo = 0, quad_charley =0, quad_elgar = 0, quad_frey = 0, quad_garen = 0, quad;
+
+	cout<<"As quadras diponíveis são: \n";
+		
+ 	cout<<" Quadra Alpha: \n Manhã das 08:30:00 as 12:00:00\n Tarde das 13:00 às 18:00\n Noite 18:00 às 22:00\n\n";
+ 	cout<<" Quadra Bravo : \n Manhã 08:30:00 as 12:00:00\n Tarde das 13:00 às 18:00\n Noite 18:00 às 22:00\n\n";
+ 	cout<<" Quadra Charley: \n Manhã 08:30:00 as 12:00:00\n Tarde das 13:00 às 18:00\n Noite 18:00 às 22:00\n\n";
+ 	cout<<" Quadra Elgar: \n Manhã 08:30:00 as 12:00:00\n Tarde das 13:00 às 18:00\n Noite 18:00 às 22:00\n\n";
+ 	cout<<" Quadra Frey: \n Manhã 08:30:00 as 12:00:00\n Tarde das 13:00 às 18:00\n Noite 18:00 às 22:00\n\n";
+ 	cout<<" Quadra Garen: \n Manhã 08:30:00 as 12:00:00\n Tarde das 13:00 às 18:00\n Noite 18:00 às 22:00\n\n";
+ 	
+               
+return 0;
+
+
+}
